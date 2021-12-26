8b764e64b640423aef9a049c8c9336e583988b10
hall-of-code <m81b4ck2@gmail.com>
2021-12-26 12:28:08 +0100

    ja

---
+++ src/DiscordLaravelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace hallofcode/discord-laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd('Hello');
+    }
+}
