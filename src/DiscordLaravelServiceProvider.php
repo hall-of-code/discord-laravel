d56361d9c1c7869ea998d35627315f04d1b0e89e
hall-of-code <m81b4ck2@gmail.com>
2021-12-26 12:39:59 +0100

    javbd

--- src/DiscordLaravelServiceProvider.php
+++ src/DiscordLaravelServiceProvider.php
@@ -4,7 +4,7 @@ namespace hallofcode\discordLaravel;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class DiscordLaravelServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
